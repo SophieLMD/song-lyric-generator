800124d393550b9731582680bef65ae73aca42d7
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-03-27 09:16:45 +0000

    Added basic javascript/form handling, fixed bug in HTML form and input submit, added typewriter plugin, adjusted CSS following bug fixing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function generateSong(event) {
+  event.preventDefault();
+  new Typewriter("#song", {
+    strings: "Even old New York, was once New Amsterdam",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let songFormElement = document.querySelector("#song-generator-form");
+songFormElement.addEventListener("submit", generateSong);
