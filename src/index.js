5ddf71b2484b05e8d46f645e22d6d9cff5ebabc4
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-03-28 08:33:13 +0000

    Fixed bug in javascript, adjusted context instructions for AI, altered CSS

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ function displaySong(response) {
 }
 function generateSong(event) {
   event.preventDefault();
-  let instructionsInput = document.querySelector("#user-instructions");
+  let instructionsInput = document.querySelector("#user-input");
   let apiKey = "tfb34aff78c33f3d839do95056440025";
   let context =
-    "You are an expert in music and writing song lyrics. You like to write humorous song lyrics. Your task is to write a 5 line song verse which is written in basic html. Follow the user instructions which will define the topic of your song and do NOT mention HTMLInputElement. Separate each line of the song with <br/>. Please include your signature of 'SheCodes AI' in <strong> at the end of your song";
+    "You are an expert in music and writing song lyrics. You like to write humorous song lyrics. Your task is to write a 5 line song verse which is written in basic html and do not include a title. Follow the user instructions which will define the topic of your song and do NOT use the word html in your response. Separate each line of the song with <br/>. Please include your signature of 'SheCodes AI' in <strong><strong> at the end of your song";
   let prompt = `User instructions: Generate a humorous song about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displaySong);
